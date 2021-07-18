1b1ee81dc3a0c9c182adf542d9d7917a8b10b009
Shun Cheung <scheung@qmo.io>
2021-07-18 18:44:40 -0400

    feat(getCurrentLocation): not fetching permission

--- src/utils/getCurrentLocation.ts
+++ src/utils/getCurrentLocation.ts
@@ -1,13 +1,9 @@
-import { Alert } from "react-native";
-import * as Location from "expo-location";
+import { getCurrentPositionAsync } from "expo-location";
 
 const getCurrentLocation = async () => {
-  let { status } = await Location.requestForegroundPermissionsAsync();
-  if (status !== "granted") {
-    Alert.alert("Location required", "Location is required to play this game");
-    throw new Error("Not granted");
-  }
-  const location = await Location.getCurrentPositionAsync({});
+  const location = await getCurrentPositionAsync({});
+  if (!location) throw new Error("Could not retrieve location");
+
   return {
     latitude: location.coords.latitude,
     longitude: location.coords.longitude,
