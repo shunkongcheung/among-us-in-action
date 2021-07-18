96c62dd9a852159bfa99f7551445e0cb050db86c
Shun Cheung <scheung@qmo.io>
2021-07-18 18:44:28 -0400

    feat(Map): fetch current location, handle compass

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,13 @@
 import React, { memo } from "react";
 import { Box, Center, Spinner } from "native-base";
+import {
+  requestForegroundPermissionsAsync,
+  watchHeadingAsync,
+} from "expo-location";
 import MapView, { Marker } from "react-native-maps";
 
 import { getCurrentLocation } from "../utils";
+import { Alert } from "react-native";
 
 export interface Location {
   latitude: number;
@@ -26,8 +31,10 @@ export interface MarkerProps {
 }
 
 interface MapProps {
-  region: Region;
+  initialRegion?: Region;
   isCurrentRegion?: boolean;
+  is3d?: boolean;
+  isCampassed?: boolean;
   markers?: Array<MarkerProps>;
   onLongPress?: (location: Location) => any;
   onRegionChange: (region: Region | RegionCallback) => any;
@@ -35,12 +42,15 @@ interface MapProps {
 
 const Map: React.FC<MapProps> = ({
   isCurrentRegion,
-  region,
+  isCampassed = false,
+  is3d = false,
+  initialRegion,
   markers = [],
   onLongPress,
   onRegionChange,
 }) => {
-  const [ready, setReady] = React.useState(!isCurrentRegion);
+  const [region, setRegion] = React.useState(initialRegion);
+  const mapRef = React.useRef<MapView>(null);
 
   const onLongPressLocal = React.useCallback(
     ({ nativeEvent }) => {
@@ -50,29 +60,65 @@ const Map: React.FC<MapProps> = ({
     [onLongPress]
   );
 
+  React.useEffect(() => {}, [isCurrentRegion, onRegionChange]);
+
   React.useEffect(() => {
     const fetchCurrent = async () => {
+      // get current location
       const region = await getCurrentLocation();
-      onRegionChange((o: Region) => ({ ...o, ...region }));
-      setReady(true);
+
+      // default map size
+      setRegion({ ...region, latitudeDelta: 0.00922, longitudeDelta: 0.00421 });
     };
 
-    if (!!isCurrentRegion) fetchCurrent();
-  }, [isCurrentRegion, onRegionChange]);
+    (async () => {
+      // fetch permission before all operation
+      const { status } = await requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission to access location was denied");
+        return;
+      }
+
+      // if location should be set to current
+      if (!!isCurrentRegion) fetchCurrent();
+
+      // if is directed
+      if (isCampassed) {
+        watchHeadingAsync(({ magHeading }) => {
+          if (mapRef.current) mapRef.current.setCamera({ heading: magHeading });
+        });
+      }
+    })();
+  }, [isCurrentRegion, isCampassed]);
 
   return (
     <Box flex={1}>
-      <MapView
-        style={{ width: "100%", height: "100%" }}
-        region={region}
-        onRegionChange={onRegionChange}
-        onLongPress={onLongPressLocal}
-      >
-        {markers.map((props, idx) => (
-          <Marker key={`Marker-${idx}`} {...props} />
-        ))}
-      </MapView>
-      {!ready && (
+      {!!region ? (
+        <MapView
+          style={{ width: "100%", height: "100%" }}
+          initialCamera={{
+            altitude: 1094,
+            pitch: is3d ? 55 : 0,
+            heading: 0,
+            center: region!,
+            zoom: 1,
+          }}
+          onRegionChange={onRegionChange}
+          onLongPress={onLongPressLocal}
+          ref={mapRef}
+          // ref={async (ref) => {
+          //   (mapRef as any).current = ref;
+          //   if (ref) {
+          //     const camera = await ref.getCamera();
+          //     setCamera(camera);
+          //   }
+          // }}
+        >
+          {markers.map((props, idx) => (
+            <Marker key={`Marker-${idx}`} {...props} />
+          ))}
+        </MapView>
+      ) : (
         <Center
           position="absolute"
           left={0}
