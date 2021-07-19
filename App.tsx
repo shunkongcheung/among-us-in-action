96b48881de74b3db8a691024c2659c1e7da9fc51
Shun Cheung <scheung@qmo.io>
2021-07-19 17:54:15 -0400

    feat(App.tsx): useUserState

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Alert } from "react-native";
 import { NativeBaseProvider, theme, extendTheme } from "native-base";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-import { requestForegroundPermissionsAsync } from "expo-location";
-
-import { UserContext } from "./src/hooks/useUserContext";
+import { useRoomState, RoomContext } from "./src/hooks/useRoomContext";
+import { useUserState, UserContext } from "./src/hooks/useUserContext";
 import Navigations from "./src/navigations";
 import typeDefs from "./src/typeDefs";
-import { Player } from "./src/types";
 
 const client = new ApolloClient({
   uri: "https://among-us.crestedmyna.com/graphql",
@@ -17,33 +14,22 @@ const client = new ApolloClient({
 });
 
 export default function App() {
-  const [user, setUser] = React.useState<Player>({
-    id: -1,
-    name: "",
-    color: "",
-    hat: "",
-  });
   const newTheme = extendTheme({
     colors: {
       primary: theme.colors.indigo,
     },
   });
 
-  React.useEffect(() => {
-    (async () => {
-      const { status } = await requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        Alert.alert("Permission to access location was denied");
-        return;
-      }
-    })();
-  }, []);
+  const user = useUserState();
+  const room = useRoomState(user.id);
 
   return (
     <ApolloProvider client={client}>
       <NativeBaseProvider theme={newTheme}>
-        <UserContext.Provider value={{ ...user, setUser }}>
-          <Navigations />
+        <UserContext.Provider value={user}>
+          <RoomContext.Provider value={room}>
+            <Navigations />
+          </RoomContext.Provider>
         </UserContext.Provider>
       </NativeBaseProvider>
     </ApolloProvider>
