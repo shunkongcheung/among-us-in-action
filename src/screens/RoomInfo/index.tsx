5e792aff0939ae32abfbdf73cdadc183abb35577
Shun Cheung <scheung@qmo.io>
2021-07-19 18:06:05 -0400

    feat(RoomInfo): template

---
+++ src/screens/RoomInfo/index.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+import { Box } from "native-base";
+
+const RoomInfo: React.FC = () => {
+  return (
+    <>
+      <Box>hello room info</Box>
+    </>
+  );
+};
+
+export default memo(RoomInfo);
