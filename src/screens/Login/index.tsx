618e07f301e7977d84c6e070dbb4059f12b1507e
Shun Cheung <scheung@qmo.io>
2021-07-19 15:25:09 -0400

    feat(Login): go to Lobby

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -37,7 +37,7 @@ const Login: React.FC = () => {
       },
       onSubmit: async (user: Player) => {
         await register(user);
-        navigate("EditGameInfo");
+        navigate("Lobby");
       },
     });
 
