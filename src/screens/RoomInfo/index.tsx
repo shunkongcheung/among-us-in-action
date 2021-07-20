8abd77a57f2f2bbde4e3212e286433bde6ab18e3
Shun Cheung <scheung@qmo.io>
2021-07-20 17:29:29 -0400

    feat(RoomInfo): show info and buttons base on room state

--- src/screens/RoomInfo/index.tsx
+++ src/screens/RoomInfo/index.tsx
@@ -1,17 +1,50 @@
 import React, { memo } from "react";
 import { Box, Button, Center, StatusBar, useTheme } from "native-base";
 import QRCode from "react-native-qrcode-svg";
+import { gql, useMutation } from "@apollo/client";
+import { useNavigation } from "@react-navigation/native";
 
 import { useRoomContext } from "../../hooks";
 import Desc from "./Desc";
 
+const START_GAME = gql`
+  mutation StartGame($roomId: Float!) {
+    startGame(roomId: $roomId) {
+      id
+    }
+  }
+`;
+
+const START_VOTE = gql`
+  mutation StartVote($roomId: Float!) {
+    startVote(roomId: $roomId) {
+      id
+    }
+  }
+`;
+
 const RoomInfo: React.FC = () => {
+  const [startGame] = useMutation(START_GAME);
+  const [startVote] = useMutation(START_VOTE);
+
   const theme = useTheme();
   const themeColor = theme.colors.primary[500];
 
+  const { navigate } = useNavigation();
   const room = useRoomContext();
 
+  const onStartGame = React.useCallback(async () => {
+    await startGame({ variables: { roomId: room!.id } });
+    navigate("RoomMap", { isPopUp: true });
+  }, [room, navigate]);
+
+  const onStartVote = React.useCallback(async () => {
+    await startVote({ variables: { roomId: room!.id } });
+  }, [room, navigate]);
+
   if (!room) return <></>;
+
+  const character = room.isImposter ? "Imposter" : "Crewmate";
 
   return (
     <>
@@ -30,9 +63,15 @@ const RoomInfo: React.FC = () => {
         <Desc title="Participants">{`${room.participants.length}/${room.game.maxParticipantCount}`}</Desc>
         <Desc title="Task">{`${room.completeCount}/${room.game.totalTask}`}</Desc>
         <Desc title="Duration (Min)">{`${room.minutePast}/${room.game.durationMinute} `}</Desc>
+        {!!room.isStarted && <Desc title="Character">{character}</Desc>}
         {!!room.isReadyToStart && (
-          <Button mt="auto" variant="solid">
+          <Button mt="auto" variant="solid" onPress={onStartGame}>
             Start
+          </Button>
+        )}
+        {!!room.isStarted && !room.isEnded && (
+          <Button mt="auto" variant="solid" onPress={onStartVote}>
+            Vote
           </Button>
         )}
       </Box>
