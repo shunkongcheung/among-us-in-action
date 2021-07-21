943d26ff9469910c2087b4482e5af7d6c6b11482
Shun Cheung <scheung@qmo.io>
2021-07-21 15:26:42 -0400

    feat(RoomParticipant): PlayerIcon

--- src/screens/RoomParticipants/ParticipantItem.tsx
+++ src/screens/RoomParticipants/ParticipantItem.tsx
@@ -10,7 +10,7 @@ import {
 } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 
-import { HATS } from "../../constants";
+import { PlayerIcon } from "../../components";
 
 interface ParticipantItemProps {
   name: string;
@@ -33,30 +33,14 @@ const ParticipantItem: React.FC<ParticipantItemProps> = ({
   isThinking,
   onVote,
 }) => {
-  const hatSource = React.useMemo(
-    () => HATS.find((itm) => itm.name === hat)!.source,
-    [hat]
-  );
-
   const isGiveUp = hat === "question-mark";
 
   return (
     <>
       <HStack py={3} px={5}>
-        <Box mt={10}>
-          <Box position="absolute" left="25%" top="-45%" zIndex={1}>
-            <Image source={hatSource} alt={`Hat of ${name}`} size="xs" />
-          </Box>
-          <Image
-            source={require("../../../assets/player-icon.png")}
-            size="sm"
-            alt={`Icon of ${name}`}
-          />
-        </Box>
+        <PlayerIcon color={color} hat={hat} size="sm" />
         <VStack justifyContent="space-evenly" mr="auto" ml={2}>
-          <Text fontWeight="bold" style={{ color }}>
-            {name}
-          </Text>
+          <Text fontWeight="bold">{name}</Text>
           {isVoting && !isGiveUp && (
             <Ionicons
               size={30}
