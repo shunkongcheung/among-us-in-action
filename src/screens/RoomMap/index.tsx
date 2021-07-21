6f7723b99d13053828f53a305797ce1e7b0c8a52
Shun Cheung <scheung@qmo.io>
2021-07-21 19:11:08 -0400

    feat(RoomControls): init

--- src/screens/RoomMap/index.tsx
+++ src/screens/RoomMap/index.tsx
@@ -1,11 +1,14 @@
-import { useRoute } from "@react-navigation/native";
 import React, { memo } from "react";
+import { useRoute } from "@react-navigation/native";
+import { Box } from "native-base";
 
 import { Map } from "../../components";
 import { Task } from "../../constants";
 
 import CharacterModal from "./CharacterModal";
-import useMap from "./useMap";
+import RoomControls from "./RoomControls";
+import useTasks from "./useTasks";
+import useKillable from "./useKillable";
 
 interface CheckPoint {
   id: number;
@@ -23,21 +26,26 @@ interface Region {
 
 interface RoomMapProps {
   checkPoints: Array<CheckPoint>;
-  region: Region;
   isImposter: boolean;
+  region: Region;
+  roomId: number;
+  survivers: Array<{ id: number }>;
 }
 
 const RoomMap: React.FC<RoomMapProps> = ({
   checkPoints,
-  region,
   isImposter,
+  region,
+  roomId,
+  survivers,
 }) => {
   const { params } = useRoute();
 
   const [isCharacterModal, setIsCharacterModal] = React.useState(
     (params as any)?.isCharacterModal || false
   );
-  const { markers } = useMap(checkPoints);
+  const { task, markers } = useTasks(roomId, checkPoints);
+  const makeKill = useKillable(roomId, survivers);
 
   return (
     <>
@@ -47,12 +55,19 @@ const RoomMap: React.FC<RoomMapProps> = ({
         handleClose={() => setIsCharacterModal(false)}
       />
       <Map
-        initialRegion={region}
+        initialRegion={{
+          ...region,
+          latitudeDelta: 0.0000922,
+          longitudeDelta: 0.0000421,
+        }}
         markers={markers}
         is3d
         isCampassed
         isShowSelf
       />
+      <Box position="absolute" right={5} top="40%" zIndex={1}>
+        <RoomControls makeKill={makeKill} isImposter={isImposter} task={task} />
+      </Box>
     </>
   );
 };
