3b12223db510f54651e4a59547e2b2ae16b0ab79
Shun Cheung <scheung@qmo.io>
2021-07-21 23:33:17 -0400

    feat(Desc): reduce space

--- src/screens/RoomInfo/Desc.tsx
+++ src/screens/RoomInfo/Desc.tsx
@@ -9,7 +9,7 @@ interface DescProps {
 const Desc: React.FC<DescProps> = ({ title, children }) => {
   const theme = useTheme();
   return (
-    <HStack justifyContent="space-between" pb={3} alignItems="flex-end">
+    <HStack justifyContent="space-between" pb={2} alignItems="flex-end">
       <Text fontWeight="bold" color={theme.colors.muted[500]}>
         {title}
       </Text>
