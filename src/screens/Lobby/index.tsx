bb36d84bb000bc648665a5df5b1e35cb41382ec4
Shun Cheung <scheung@qmo.io>
2021-07-21 23:24:56 -0400

    feat(Lobby): add swipe buttons

--- src/screens/Lobby/index.tsx
+++ src/screens/Lobby/index.tsx
@@ -1,10 +1,12 @@
 import React, { memo } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
+  Box,
   Divider,
   HStack,
   Input,
   SearchIcon,
+  Text,
   FlatList,
   useTheme,
 } from "native-base";
@@ -13,6 +15,7 @@ import { AppBar } from "../../components";
 import OptionItem from "./OptionItem";
 import JoinRoomModal from "./JoinRoomModal";
 import { useJoin, useLocalGames } from "../../hooks";
+import { SwipeRow } from "react-native-swipe-list-view";
 
 interface Game {
   id: number;
@@ -21,7 +24,7 @@ interface Game {
 
 const Lobby: React.FC = () => {
   const { navigate } = useNavigation();
-  const { games } = useLocalGames();
+  const { games, removeGame } = useLocalGames();
   const { joinGame } = useJoin();
   const theme = useTheme();
 
@@ -49,7 +52,7 @@ const Lobby: React.FC = () => {
     <>
       <JoinRoomModal open={open} handleClose={() => setOpen(false)} />
       <AppBar isBackable>Lobby</AppBar>
-      <HStack alignItems="center" py={2} px={2}>
+      <HStack alignItems="center" py={2} px={2} backgroundColor="white">
         <SearchIcon size={8} color={theme.colors.primary[400]} />
         <Input
           borderWidth={0}
@@ -57,25 +60,54 @@ const Lobby: React.FC = () => {
           onChangeText={(j: string) => setSearch(j.toLowerCase())}
         />
       </HStack>
+      <Divider />
 
-      <FlatList
-        data={[
-          {
-            id: "create",
-            name: "Create New Game",
-            onPress: () => navigate("EditGameInfo"),
-          },
-          { id: "join", name: "Join Room", onPress: () => setOpen(true) },
-          ...gameData,
-        ]}
-        renderItem={({ item }) => (
-          <>
-            <OptionItem {...item} />
-            <Divider />
-          </>
-        )}
-        keyExtractor={(item) => item.id}
-      />
+      <Box mt={5}>
+        <Divider />
+        <FlatList
+          data={[
+            {
+              id: "create",
+              name: "Create New Game",
+              onPress: () => navigate("EditGameInfo"),
+            },
+            { id: "join", name: "Join Room", onPress: () => setOpen(true) },
+            ...gameData,
+          ]}
+          renderItem={({ item }) => {
+            if (isNaN(item.id))
+              return (
+                <Box backgroundColor="white">
+                  <OptionItem {...item} />
+                  <Divider />
+                </Box>
+              );
+            return (
+              <SwipeRow
+                disableLeftSwipe
+                onSwipeValueChange={({ value }) => {
+                  if (value > 150) removeGame(item.id);
+                }}
+              >
+                <Box
+                  backgroundColor="red"
+                  justifyContent="center"
+                  height="100%"
+                  width="50%"
+                  pl={2}
+                >
+                  <Text style={{ color: "white" }}>Delete</Text>
+                </Box>
+                <Box backgroundColor="white">
+                  <OptionItem {...item} />
+                  <Divider />
+                </Box>
+              </SwipeRow>
+            );
+          }}
+          keyExtractor={(item) => item.id}
+        />
+      </Box>
     </>
   );
 };
